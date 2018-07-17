54d4f4985ce811be350818abbc23410e8a1a886d
moon by the stars <597799725@qq.com>
2018-07-17 23:30:20 +0800

    Update smallCS.m

--- toy_example/smallCS.m
+++ toy_example/smallCS.m
@@ -1,7 +1,14 @@
+
+%{
+    input：pool：The number of pools.
+           p：Determine the proportion of 1 in the M matrix.
+    output：recoverXX：inference X.
+%}
+
 function smallCS(pool,p)
 pool = str2num(pool)
-
 p = str2num(p)
+
 original = importdata('pan_expression.txt');
 X = [original.data];
 
@@ -11,12 +18,11 @@ M = double(rand(pool,64)<p);
 recoverX = [];
 for i = 1:size(X,2)
 	y = M*X(:,i);          
-	x0 = M'*y; 
+	[w, hcond] = linsolve(M*M',y);
+	x0 = M'*w; 
 	xp = l1eq_pd(x0, M, [], y);
 	recoverX = [recoverX xp];
 end
 
 fname = ['pool_',num2str(pool),'_p_',num2str(p),'.mat'];
-%save fname -ascii recoverX
 save(fname,'recoverX','M','-mat');
-
